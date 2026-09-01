20fbb3a2572f934ac6736d2bdda701de333acc59
superzakk963 <zakknasrul@gmail.com>
2026-09-01 11:54:52 +0700

    mengubah <div class=logo-text>NihonGo!</div> ke <div class=logo-text>NihonGo! - GitHub</div>

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ if (isLoggedIn()) redirect('user/dashboard.php');
     
     <div class="hero">
         <div class="logo">🎌</div>
-        <div class="logo-text">NihonGo!</div>
+        <div class="logo-text">NihonGo! - GitHub</div>
         <div class="logo-jp">日本語を学ぼう</div>
         <p class="hero-desc">Belajar bahasa Jepang dengan cara yang menyenangkan! Seperti bermain game, kumpulkan XP, jaga streak harian, dan kuasai bahasa Jepang. Ayo mulai sekarang!</p>
         
